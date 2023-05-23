bd67f16fb64d3c0723b80166a9993c62cb3715e6
ksh25 <ksh251024@naver.com>
2023-05-23 19:33:28 +0900

    직접풀어보기 4-4시작버튼 Switch로 변경, 선택완료버튼 삭제

--- app/src/main/java/kr/hs/emirim/sohee/petphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/sohee/petphoto/MainActivity.kt
@@ -6,10 +6,9 @@ import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var checkstart:CheckBox
+    lateinit var checkstart:Switch
     lateinit var rg:RadioGroup
     lateinit var linear:LinearLayout
-    lateinit var btnDone:Button
     lateinit var imgv:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         checkstart=findViewById(R.id.check_start)
         rg=findViewById(R.id.rg)
         linear=findViewById(R.id.linear)
-        btnDone=findViewById(R.id.btn_done)
         imgv=findViewById(R.id.imgv)
         linear.visibility=View.INVISIBLE
 
@@ -29,13 +27,13 @@ class MainActivity : AppCompatActivity() {
                 linear.visibility = View.INVISIBLE
             }
         }
-        btnDone.setOnClickListener{
-            when(rg.checkedRadioButtonId){
-                R.id.radio_dog -> imgv.setImageResource(R.drawable.dog)
-                R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
-                R.id.radio_deer -> imgv.setImageResource(R.drawable.deer)
-                else-> Toast.makeText(applicationContext,"라디오버튼이 선택되지 않았습니다.",Toast.LENGTH_SHORT).show()
-            }
-        }
+
+       rg.setOnCheckedChangeListener {compoundButton,b->
+           when(rg.checkedRadioButtonId){
+               R.id.radio_dog->imgv.setImageResource(R.drawable.dog)
+               R.id.radio_cat->imgv.setImageResource(R.drawable.cat)
+               R.id.radio_deer->imgv.setImageResource(R.drawable.deer)
+           }
+       }
     }
 }
